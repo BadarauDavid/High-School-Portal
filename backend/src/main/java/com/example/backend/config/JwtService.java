f911f1fe550b730e5af5b9f7fa66ac53ea145f43
BadarauDavid <badaraudavidiulian@gmail.com>
2024-01-15 18:48:58 +0200

    add role in jwt

--- backend/src/main/java/com/example/backend/config/JwtService.java
+++ backend/src/main/java/com/example/backend/config/JwtService.java
@@ -37,9 +37,10 @@ public class JwtService {
             .builder()
             .setClaims(extraClaims)
             .setSubject(userDetails.getUsername())
+            .claim("role",userDetails.getAuthorities())
             .setIssuedAt(new Date(System.currentTimeMillis()))
             .setExpiration(new Date(System.currentTimeMillis()+1000*60*24))
-            .signWith(getSignInKey(), SignatureAlgorithm.HS384)
+            .signWith(getSignInKey(), SignatureAlgorithm.HS256)
             .compact();
     }
 
