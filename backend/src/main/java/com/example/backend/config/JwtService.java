1426782ad3319ad40b8f96f893a55629422be666
BadarauDavid <badaraudavidiulian@gmail.com>
2024-01-16 00:00:04 +0200

    update

--- backend/src/main/java/com/example/backend/config/JwtService.java
+++ backend/src/main/java/com/example/backend/config/JwtService.java
@@ -37,7 +37,6 @@ public class JwtService {
             .builder()
             .setClaims(extraClaims)
             .setSubject(userDetails.getUsername())
-            .claim("role",userDetails.getAuthorities())
             .setIssuedAt(new Date(System.currentTimeMillis()))
             .setExpiration(new Date(System.currentTimeMillis()+1000*60*24))
             .signWith(getSignInKey(), SignatureAlgorithm.HS256)
