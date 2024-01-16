19c88aed79afc420bc4a2324469ea1debe3315e7
BadarauDavid <badaraudavidiulian@gmail.com>
2024-01-16 23:10:35 +0200

    add create account

--- frontend/src/pages/CreateAccount.jsx
+++ frontend/src/pages/CreateAccount.jsx
@@ -1,5 +1,116 @@
+import { useNavigate } from "react-router-dom";
+import axios from "axios";
+
+
 export default function CreateAccount(){
-    return(
-        <h1>este account</h1>
-    )
+    const navigate = useNavigate();
+
+  
+    const onSubmit = async (values) => {
+
+  
+      try {
+        await axios.post(
+          `http://localhost:8080/api/auth/register`,
+          values
+        );
+        
+        navigate("/adminPanel");
+      } catch (err) {
+        console.log(err);
+      }
+    };
+  
+    const onSave = (e) => {
+      e.preventDefault();
+      const formData = new FormData(e.target);
+      const highSchoolInfo = {
+        firstName: formData.get("firstName"),
+        lastName: formData.get("lastName"),
+        email: formData.get("email"),
+        password: formData.get("password"),
+        role: formData.get("role")
+      };
+      onSubmit(highSchoolInfo);
+    };
+  
+    return (
+      <form onSubmit={onSave} style={{ marginTop: 175 }}>
+        <div className="container py-3 h-100">
+          <div className="row d-flex justify-content-center align-items-center h-100">
+            <div className="col-12 col-md-8 col-lg-6 col-xl-5">
+              <div className="card shadow-2-strong">
+                <div className="card-body p-5 text-center">
+                  <h1 className="mb-3">Create Account</h1>
+  
+                  <div className="form-outline mb-4">
+                    <input
+                    required
+                      type="text"
+                      className="form-control"
+                      id="firstName"
+                      name="firstName"
+                      placeholder="FirstName"
+                    />
+                  </div>
+  
+                  <div className="form-outline mb-4">
+                    <input
+                    required
+                      type="text"
+                      className="form-control"
+                      id="lastName"
+                      name="lastName"
+                      placeholder="LastName"
+                    />
+                  </div>
+
+                  <div className="form-outline mb-4">
+                    <input
+                    required
+                      type="email"
+                      className="form-control"
+                      id="email"
+                      name="email"
+                      placeholder="Email"
+                    />
+                  </div>
+
+                  <div className="form-outline mb-4">
+                    <input
+                    required
+                      type="password"
+                      className="form-control"
+                      id="password"
+                      name="password"
+                      placeholder="Password"
+                    />
+                  </div>
+  
+                  <div className="form-outline mb-4">
+                    <select class="form-select form-select-lg mb-3" name="role" aria-label="Large select example">
+  <option  selected disabled >Select Role</option>
+
+ <option  value="ROLE_STUDENT">Student</option>
+ <option value="ROLE_TEACHER">Teacher</option>
+
+ 
+
+</select>
+                    </div>
+
+                  <button
+                    className="btn btn-primary btn-lg btn-block"
+                    type="submit"
+                  >
+                    Create
+                  </button>
+  
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </form>
+    );
 }
--- frontend/src/pages/CreateClassroom.jsx
+++ frontend/src/pages/CreateClassroom.jsx
@@ -64,7 +64,7 @@ export default function CreateClassroom(){
             <div className="col-12 col-md-8 col-lg-6 col-xl-5">
               <div className="card shadow-2-strong">
                 <div className="card-body p-5 text-center">
-                  <h1 className="mb-3">Create HighSchool</h1>
+                  <h1 className="mb-3">Create Classroom</h1>
   
                   <div className="form-outline mb-4">
                     <input
